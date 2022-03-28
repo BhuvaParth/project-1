042547ae2f091bd93a22b31f11ed3a5d87420913
BhuvaParth <parth.d.bhuva2331@gmail.com>
2022-03-28 09:20:36 +0530

    NEW

--- src/App.js
+++ src/App.js
@@ -2,23 +2,74 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let data = [
+    {
+      name: "amit",
+      age: 35,
+      salary: 40000,
+      bonus: 1000,
+      status: true
+    },
+    {
+      name: "ajay",
+      age: 25,
+      salary: 38000,
+      bonus: 2000,
+      status: false
+    },
+    {
+      name: "mayur",
+      age: 23,
+      salary: 50000,
+      bonus: 500,
+      status: true
+    },
+    {
+      name: "jay",
+      age: 29,
+      salary: 35000,
+      bonus: 600,
+      status: true
+    },
+    {
+      name: "raj",
+      age: 33,
+      salary: 22000,
+      bonus: 2000,
+      status: true
+    },
+  ];
+
+  let filterData = data.filter((x, i) => (x.salary > 35000 && x.age > 25  || x.bonus > 500))
+
+  // console.log(filterData); 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React1
-        </a>
-      </header>
-    </div>
+    <table border="1px">
+      <tr>
+        <th>name</th>
+        <th>age</th>
+        <th>salary</th>
+        <th>bonus</th>
+        <th>status</th>
+      </tr>
+
+      {
+        filterData.map((x, i) => {
+          return (
+            <>
+              <tr>
+                <td>{x.name}</td>
+                <td>{x.age}</td>
+                <td>{x.salary}</td>
+                <td>{x.bonus}</td>
+                <td>{x.status}</td>
+              </tr>
+            </>
+          )
+        })
+      }
+    </table>
   );
 }
 
