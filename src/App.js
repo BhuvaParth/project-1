6adb8bc2b60ff2a55794fde96cdf749fbbbaf823
BhuvaParth <parth.d.bhuva2331@gmail.com>
2022-04-02 14:34:42 +0530

    arry

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import logo from './logo.svg';
 import React from 'react';
-import Employee from './New folder (2)/employee';
-import Madicine from './New folder (2)/madicine';
+import Employee from './New folder (2)/Employee';
+import Madicine from './New folder (2)/Madicine';
 
 function App(props) {
+
+  let arr1 = [10, 20, 30];
+  let arr2 = [40, 50];
+
+  let arr3 = [...arr1, ...arr2];
+
+  console.log(arr3);
   return (
     <div>
-      <Employee />
-      <Madicine />
-      
+      {/* <Employee /> */}
+      {/* <Madicine /> */}
+
     </div>
   );
 }
