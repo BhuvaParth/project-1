4519e1ece532ee3eb0fbf6cfb2021a4b72d336b9
BhuvaParth <parth.d.bhuva2331@gmail.com>
2022-04-14 11:29:12 +0530

    new 4.4.5

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
 import City from './containers/City';
 import Count from './containers/Count';
 import Country from './containers/State';
@@ -7,22 +9,51 @@ import Madicine from './containers/Madicine';
 import State from './containers/State';
 import Clock from './containers/Clock';
 import ClockFun from './containers/ClockFun';
+import Home from './containers/Home/Home';
+import Loading from './components/Loading/Loading';
 
-function App() {
+const HomeWithLoading = Loading(Home)
+
+function App(props) {
+    const [loading, setLoading] = useState(false); 
+    const [data, setData] = useState([]);
+
+    const userData = [
+      {
+        id: 101,
+        name: 'amit'
+      },
+      {
+        id: 105,
+        name: 'ajay'
+      }
+    ]
+
+    useEffect (
+      () => {
+        setLoading(true)
+        setTimeout(() => {setLoading(false); setData(userData)}, 2000)
+      },
+    [])
+
   return (
-    <div>
+    <div class="text-center fs-3">
+      <HomeWithLoading
+      isLoading= {loading}
+      data={data}
+
+      />
 
       {/* <Madicine /> */}
-
       {/* <Employee /> */}
-      
       {/* <Country /> */}
       {/* <City /> */}
       {/* <Array /> */}
       {/* <Count /> */}
       {/* <State /> */}
       {/* <Clock /> */}
-      <ClockFun />
+      {/* <ClockFun /> */}
+
     </div>
   );
 }
---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Loading(Component) {
+    return function withLoadingComponent({ isLoading, data }) {
+        if (isLoading) {
+            return (
+                <div class="position-absolute top-50 start-50 translate-middle  fs-5">
+                    <div class="spinner-border" role="status">
+                        <span class="visually-hidden" >Loading...</span>
+                    </div>
+                </div>
+            )
+        } else {
+            return (
+                <Component data={data} />
+            );
+        }
+    }
+}
+
+export default Loading;
